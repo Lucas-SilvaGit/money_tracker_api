4cde6a7f06a7310eb95593ce47bd4790d8e62dd6
Jose Lucas <lucasnba2324@gmail.com>
2023-08-06 19:47:26 -0300

    adding unit tests for the model transaction and fix factory transaction

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,11 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  it { is_expected.to validate_presence_of(:description) }
-  it { is_expected.to validate_presence_of(:transaction_date) }
-  it { is_expected.to validate_presence_of(:value) }
-  it { is_expected.to validate_presence_of(:status) }
-  it { is_expected.to validate_presence_of(:category) }
+  describe " Financial transactions" do
+    context "when has attributes" do
+      it "is valid with valid attributes" do
+        transaction = build(:transaction)
+        expect(transaction).to be_valid
+      end
+    end
 
-  it { is_expected.to it belong_to(:transactable) } 
+    context "when not has attributes" do
+      it "is invalid without a description" do
+        transaction = build(:transaction, description: nil)
+        expect(transaction).to be_invalid
+      end
+    
+      it "is invalid without a transaction_date" do
+        transaction = build(:transaction, transaction_date: nil)
+        expect(transaction).to be_invalid
+      end
+    
+      it "is invalid without a value" do
+        transaction = build(:transaction, value: nil)
+        expect(transaction).to be_invalid
+      end
+    
+      it "is invalid without a status" do
+        transaction = build(:transaction, status: nil)
+        expect(transaction).to be_invalid
+      end
+    
+      it "is invalid without a category" do
+        transaction = build(:transaction, category: nil)
+        expect(transaction).to be_invalid
+      end
+    end
+  end
 end
