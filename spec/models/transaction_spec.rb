f68bd82500328b1b445986fc9ff506b0ecbbd019
Jose Lucas <lucasnba2324@gmail.com>
2023-08-05 23:49:26 -0300

    creanting tests validation for the model Transaction

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:transaction_date) }
+  it { is_expected.to validate_presence_of(:value) }
+  it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to validate_presence_of(:category) }
+
+  it { is_expected.to it belong_to(:transactable) } 
 end
