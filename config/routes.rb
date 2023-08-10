b5272e210d0d19fedde5bc28452d32cfb7ef69d3
Jose Lucas <lucasnba2324@gmail.com>
2023-08-09 21:24:06 -0300

    adding routes for the new controllers expenses e incomes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :transactions, except: [:new, :edit]
+      resources :expenses, except: [:new, :edit]
+      resources :incomes, except: [:new, :edit]
     end
   end
 end
